19705f9fdc394f7200cad606372c8a90fbfbd572
RenugaR <pavi.renu95@gmail.com>
2021-10-05 08:04:41 +0530

    Added RBI Bank Project

---
+++ src/com/hdfc/HdfcBank.java
@@ -0,0 +1,27 @@
+package com.hdfc;
+
+//className-->HdfcBank
+public class HdfcBank {
+	// Method
+	// Type method name and click control+spacebar then click enter
+	public void deposit() {
+		// business logic
+		System.out.println("Deposit");// To print the output
+	}
+
+	public void withDraw() {
+		System.out.println("WithDraw");
+	}
+
+	// To run the program-->right click class-->run as-->java application
+	public static void main(String[] args) {
+		// Object creation-->className refName=new className();
+		HdfcBank hdfc = new HdfcBank();
+		// refName-->hdfc
+		// new-->keyword-->will allocate memory at run time
+
+		// method calling-->refName.methodName();
+		hdfc.deposit();
+		hdfc.withDraw();
+	}
+}
